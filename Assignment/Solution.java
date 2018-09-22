57d685a9937d8b2f19eb118f3546e7a22691d838
vickychowdary <vicky4409@gmail.com>
2018-09-22 10:45:49 +0530

    first commit

---
+++ Assignment/Solution.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+
+public class Solution {
+	/*
+	Do not modify this main function.
+	*/
+	
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);      
+        int base = s.nextInt();
+        int exponent = s.nextInt();
+        int result=(int)Math.pow(base,exponent);
+        System.out.println(result);
+	}
+	/*
+	Need to write the power function and print the output.
+	*/
+}
